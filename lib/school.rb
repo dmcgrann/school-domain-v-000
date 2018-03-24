97bf670b2900f597d4fad691b260dcdd15fc8d07
David McGrann <dpmcgrann@gmail.com>
2018-03-24 19:03:32 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,9 +1,7 @@
 class School
   attr_accessor :name, :grade_level, :student
-  ROSTER = {"#{grade_level}" => "#{student}"}
-  def initialize(name, grade_level, student)
+  ROSTER = {}
+  def initialize(name)
     @name = name
-    @grade_level = grade_level
-    @student = studetnt
   end
 end
